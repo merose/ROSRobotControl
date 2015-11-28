8fdff075dcd8244c9f7e7ad7276cb055ea1fe863
Mark Rose <markrose@acm.org>
2015-11-27 19:40:28 -0800

    Moved PID controller into the Arduino node, and changed parameters to match.

--- ROSRomeoNode.ino
+++ ROSRomeoNode.ino
@@ -6,6 +6,8 @@
 // Needed on Leonardo to force use of USB serial.
 #define USE_USBCON
 
+#include <SimplePID.h>
+
 #include <ros.h>
 #include <std_msgs/Int16.h>
 #include <std_msgs/Int32.h>
@@ -34,17 +36,30 @@ ros::Publisher lwheelPub("lwheel", &lwheelMsg);
 std_msgs::Int16 rwheelMsg;
 ros::Publisher rwheelPub("rwheel", &rwheelMsg);
 
-std_msgs::Float32 lwheelRateMsg;
-ros::Publisher lwheelRatePub("lwheel_rate", &lwheelRateMsg);
+std_msgs::Float32 lwheelVelocityMsg;
+ros::Publisher lwheelVelocityPub("lwheel_velocity", &lwheelVelocityMsg);
 
-std_msgs::Float32 rwheelRateMsg;
-ros::Publisher rwheelRatePub("rwheel_rate", &rwheelRateMsg);
+std_msgs::Float32 rwheelVelocityMsg;
+ros::Publisher rwheelVelocityPub("rwheel_velocity", &rwheelVelocityMsg);
 
-void lmotorCmdCallback(const std_msgs::Float32& cmdMsg);
-ros::Subscriber<std_msgs::Float32> lmotorCmdSub("lmotor_cmd", &lmotorCmdCallback);
+void lwheelTargetCallback(const std_msgs::Float32& cmdMsg);
+ros::Subscriber<std_msgs::Float32> lwheelTargetSub("lwheel_vtarget", &lwheelTargetCallback);
 
-void rmotorCmdCallback(const std_msgs::Float32& cmdMsg);
-ros::Subscriber<std_msgs::Float32> rmotorCmdSub("rmotor_cmd", &rmotorCmdCallback);
+void rwheelTargetCallback(const std_msgs::Float32& cmdMsg);
+ros::Subscriber<std_msgs::Float32> rwheelTargetSub("rwheel_vtarget", &rwheelTargetCallback);
+
+// Ziegler-Nichols tuning. See this Wikipedia article for details:
+//     https://en.wikipedia.org/wiki/PID_controller#Loop_tuning
+// Ku and Tu were determined by setting Ki and Kd to zero, then increasing
+// Kp until steady oscillation occurs. Tu is the oscillation wavelength.
+const float Ku = .19;
+const float Tu = .23;
+const float Kp = 0.45*Ku;
+const float Ki = 1.2*Kp/Ku;
+const float Kd = Kp*Tu/8;
+
+SimplePID leftController = SimplePID(Kp, Ki, Kd);
+SimplePID rightController = SimplePID(Kp, Ki, Kd);
 
 volatile long lwheel = 0;
 volatile long rwheel = 0;
@@ -52,19 +67,28 @@ volatile long rwheel = 0;
 long lastLwheel = 0;
 long lastRwheel = 0;
 
-float lwheelRate = 0.0;
-float rwheelRate = 0.0;
+// Target motors speeds in ticks per second.
+int lwheelTargetRate = 0;
+int rwheelTargetRate = 0;
 
-int lmotorCmd = 0;
-int rmotorCmd = 0;
+// The interval between motor control steps.
+int controlDelayMillis;
 
-int lmotorMin;
-int rmotorMin;
+// The number of encoder ticks per meter.
+int ticksPerMeter;
 
-int motorCmdTimeout;
+// The number of milliseconds without a velocity target when the robot
+// will automatically turn off the motors.
+int vtargetTimeoutMillis;
 
 unsigned long lastLoopTime;
 unsigned long lastMotorCmdTime;
+
+int leftMotorCmd = 0;
+int rightMotorCmd = 0;
+
+// Minimum motor control value. Motor output below this will stall.
+const int MIN_MOTOR_CMD = 60;
 
 void setup()
 {
@@ -74,19 +98,37 @@ void setup()
 
   attachInterrupt(digitalPinToInterrupt(M1_A), leftAChange, CHANGE);
   attachInterrupt(digitalPinToInterrupt(M2_A), rightAChange, CHANGE);
-  
+
   nh.initNode();
-
-  float motorMin;
-  nh.getParam("~lmotorMin", &motorMin);
-  lmotorMin = (int) motorMin;
-  nh.getParam("~rmotorMin", &motorMin);
-  rmotorMin = (int) motorMin;
-
-  nh.getParam("~motor_cmd_timeout", &motorCmdTimeout);
 
   nh.advertise(lwheelPub);
   nh.advertise(rwheelPub);
+  nh.advertise(lwheelVelocityPub);
+  nh.advertise(rwheelVelocityPub);
+
+  nh.subscribe(lwheelTargetSub);
+  nh.subscribe(rwheelTargetSub);
+
+  // Wait until the node has initialized before getting parameters.
+  while(!nh.connected()) {
+    nh.spinOnce();
+  }
+
+  int controlRate;
+  if (!nh.getParam("~control_rate", &controlRate)) {
+    controlRate = 50;
+  }
+  controlDelayMillis = 1000.0 / controlRate;
+  
+  if (!nh.getParam("ticks_meter", &ticksPerMeter)) {
+    ticksPerMeter = 50;
+  }
+  
+  float vtargetTimeout;
+  if (!nh.getParam("~vtarget_timeout", &vtargetTimeout)) {
+    vtargetTimeout = 2.0;
+  }
+  vtargetTimeoutMillis = vtargetTimeout * 1000;
 
   lastLoopTime = micros();
   lastMotorCmdTime = lastLoopTime;
@@ -95,6 +137,8 @@ void setup()
 // Every loop, publish the encoder and wheel rates.
 void loop()
 {
+  delay(controlDelayMillis);
+
   long curLoopTime = micros();
 
   noInterrupts();
@@ -107,41 +151,65 @@ void loop()
   lwheelPub.publish(&lwheelMsg);
   rwheelPub.publish(&rwheelMsg);
 
-  unsigned long deltaMicros = curLoopTime - lastLoopTime;
+  float dt = (curLoopTime - lastLoopTime) / 1E6;
 
-  lwheelRate = 0.5*lwheelRate + 0.5*((curLwheel - lastLwheel) / (float) deltaMicros * 1E6);
-  rwheelRate = 0.5*rwheelRate + 0.5*((curRwheel - lastRwheel) / (float) deltaMicros * 1E6);
-  lwheelRateMsg.data = lwheelRate;
-  rwheelRateMsg.data = rwheelRate;
-  lwheelRatePub.publish(&lwheelRateMsg);
-  rwheelRatePub.publish(&rwheelRateMsg);
+  float lwheelRate = ((curLwheel - lastLwheel) / dt);
+  float rwheelRate = ((curRwheel - lastRwheel) / dt);
+
+  lwheelVelocityMsg.data = lwheelRate / ticksPerMeter;
+  rwheelVelocityMsg.data = rwheelRate / ticksPerMeter;
+  lwheelVelocityPub.publish(&lwheelVelocityMsg);
+  rwheelVelocityPub.publish(&rwheelVelocityMsg);
   
-  lastLwheel = curLwheel;
-  lastRwheel = curRwheel;
+  int leftControl = leftController.getControlValue(lwheelRate, dt);
+  leftMotorCmd += min(255, leftControl);
+  leftMotorCmd = constrain(leftMotorCmd, -255, 255);
+  if (leftMotorCmd > 0) {
+    leftMotorCmd = max(leftMotorCmd, MIN_MOTOR_CMD);
+  }
+  
+  int rightControl = rightController.getControlValue(rwheelRate, dt);
+  rightMotorCmd += min(255, rightControl);
+  rightMotorCmd = constrain(rightMotorCmd, -255, 255);
+  if (rightMotorCmd > 0) {
+    rightMotorCmd = max(rightMotorCmd, MIN_MOTOR_CMD);
+  }
+
+  // Coast to a stop if target is zero.
+  if (lwheelTargetRate == 0) {
+    leftMotorCmd = 0;
+  }
+  if (rwheelTargetRate == 0) {
+    rightMotorCmd = 0;
+  }
+  
+  setSpeed(leftMotorCmd, rightMotorCmd);
   
   // Turn off motors if too much time has elapsed since last motor command.
-  if (millis() - lastMotorCmdTime > motorCmdTimeout) {
-    lmotorCmd = 0;
-    rmotorCmd = 0;
+  if (millis() - lastMotorCmdTime > vtargetTimeoutMillis) {
+    lwheelTargetRate = 0;
+    rwheelTargetRate = 0;
     setSpeed(0, 0);
   }
 
+  lastLwheel = curLwheel;
+  lastRwheel = curRwheel;
+  
+  lastLoopTime = curLoopTime;
+  
   nh.spinOnce();
-  delay(10);
 }
 
-void lmotorCmdCallback(const std_msgs::Float32& cmdMsg) {
+void lwheelTargetCallback(const std_msgs::Float32& cmdMsg) {
   lastMotorCmdTime = millis();
-  lmotorCmd = cmdMsg.data;
-
-  setSpeed(lmotorCmd, rmotorCmd);
+  lwheelTargetRate = cmdMsg.data * ticksPerMeter;
+  leftController.setSetPoint(lwheelTargetRate);
 }
 
-void rmotorCmdCallback(const std_msgs::Float32& cmdMsg) {
+void rwheelTargetCallback(const std_msgs::Float32& cmdMsg) {
   lastMotorCmdTime = millis();
-  rmotorCmd = cmdMsg.data;
-
-  setSpeed(lmotorCmd, rmotorCmd);
+  rwheelTargetRate = cmdMsg.data * ticksPerMeter;
+  rightController.setSetPoint(rwheelTargetRate);
 }
 
 void leftAChange() {
@@ -162,13 +230,6 @@ void rightAChange() {
 
 // Sets the left and right motor speeds.
 void setSpeed(int leftSpeed, int rightSpeed) {
-  if (abs(leftSpeed) < lmotorMin) {
-    leftSpeed = 0;
-  }
-  if (abs(rightSpeed) < rmotorMin) {
-    rightSpeed = 0;
-  }
-
   digitalWrite(M1_DIRECTION, (leftSpeed >= 0 ? HIGH : LOW));
   analogWrite(M1_SPEED, abs(leftSpeed));
   digitalWrite(M2_DIRECTION, (rightSpeed >= 0 ? HIGH : LOW));
